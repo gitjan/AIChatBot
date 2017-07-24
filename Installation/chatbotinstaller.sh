2285a0e982a889e4585050be00ca65cc18fda2b5
Ubuntu <ubuntu@ip-172-31-20-160.eu-central-1.compute.internal>
2017-07-24 17:10:08 +0000

    Second verson installer script

--- Installation/chatbotinstaller.sh
+++ Installation/chatbotinstaller.sh
@@ -1,2 +1,8 @@
-apt-get update
-apt-get install 
+git config --global user.name  gitjan
+git config --global user.email jan.verwaest@outlook.be
+git init
+git clone https://github.com/gitjan/AIChatBot.git
+
+
+# apt-get update
+# apt-get install package -y
